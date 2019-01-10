cf9ddb2c394ffd7547d9ff565f49a02907c754b1
Adam King <kingadam7@gmail.com>
2019-01-10 05:22:35 -0800

    Getting rooms to show from firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
+import RoomList from './components/RoomList.js';
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCQeuwe9U5ruVQoPWTkN-VxfiwCPsPoc0I",
+    authDomain: "chatroom-d73c6.firebaseapp.com",
+    databaseURL: "https://chatroom-d73c6.firebaseio.com",
+    projectId: "chatroom-d73c6",
+    storageBucket: "chatroom-d73c6.appspot.com",
+    messagingSenderId: "929590754097"
+  };
+  firebase.initializeApp(config);
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <RoomList firebase={firebase}/>
       </div>
     );
   }
