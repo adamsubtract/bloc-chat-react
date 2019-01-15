0006f46b4e73005409278be5f03a5b975985f38e
Adam King <kingadam7@gmail.com>
2019-01-15 10:01:37 -0800

    forgot to merge to master

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ class App extends Component {
     return (
       <div className="App">
         <RoomList firebase={firebase}/>
-        <MessageList firebase={firebase}/>
       </div>
     );
   }
